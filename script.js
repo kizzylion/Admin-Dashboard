8b40d4aad95c470ed8dfe954a048a93bf31cf58f
kizzylion <kztchm@gmail.com>
2023-04-02 03:51:46 +0100

    RESPONSIVE SCREEN AND MENU BUTTON FIXED

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let menuButton = document.getElementById("menu-button");
+let sideNav = document.getElementById("side-nav")
+menuButton.addEventListener("click", function(){
+    sideNav.classList.toggle("active")
+})
