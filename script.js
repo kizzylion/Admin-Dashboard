521a12c8d993752e9a9601cb66b9f9c2d9b7b8df
kizzylion <kztchm@gmail.com>
2023-04-05 17:56:00 +0100

    REBUILT UI USING BOOTSTRAP 5 FRAMEWORK

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 
+// interacative menu button
+
 let menuButton = document.getElementById("menu-button");
 let sideNav = document.getElementById("side-nav")
 menuButton.addEventListener("click", function(){
-    sideNav.classList.toggle("active")
+    sideNav.classList.toggle("activate")
 })
+
+
+// Activating Active state on sidebar nav links when clicked
+let navBtn = document.querySelectorAll("#menu-section .list-group-item");
+
+navBtn.forEach(link => {
+    link.addEventListener("click", (e)=> {
+        e.preventDefault();
+        navBtn.forEach(link => link.classList.remove("active"));
+        link.classList.add("active")
+    })
+})
+
+
+// Share your work button fixed
+
+document.addEventListener('DOMContentLoaded', function() {
+    const shareworkLabel = document.querySelector("#shareworklabel");
+    shareworkLabel.textContent = 'sharework';
+
+    window.addEventListener('resize', function () {
+        if (window.innerWidth <= 567) {
+            shareworkLabel.textContent = '+';
+        } else {
+            shareworkLabel.textContent = 'sharework';
+        }
+    });
+});
